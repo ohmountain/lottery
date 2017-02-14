648f3b4f79d43ebcde3d639f40a61e228a729e2b
renshan <1005110700@qq.com>
2017-02-14 10:26:46 +0800

    fix gitignore

--- src/LotteryBundle/Controller/DefaultController.php
+++ src/LotteryBundle/Controller/DefaultController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    /**
+     * Default action
+     */
     public function indexAction()
     {
         return $this->render('LotteryBundle:Default:index.html.twig');
